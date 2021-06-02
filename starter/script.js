6cc87f7c6879f3a2fd5a1f74fe207ad023dd82d7
acparkgh <acpark123@gmail.com>
2021-06-02 18:13:00 -0400

    JS OOP Code Challenge 2

--- starter/script.js
+++ starter/script.js
@@ -221,3 +221,39 @@ console.log(sarah);
 // console.log(mercedes.speed);
 // mercedes.accelerate();
 // console.log(mercedes.speed);
+
+////////////////// Code Challenge 2 //////////////////////////////////
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  };
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.car} is going at ${this.speed}`);
+  };
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.car} is going at ${this.speed}`);
+  };
+
+  get speedUS() {
+    return this.speed / 1.6;
+  };
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  };
+
+};
+
+const ford = new CarCl("Ford", 120);
+console.log(ford);
+console.log(ford.speed);
+console.log(ford.speedUS);
+console.log(ford);
+ford.speedUS = 100;
+console.log(ford);
